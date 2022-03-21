194a2ae48699ffc9e2428f55395faba105d66d65
wqross <wqr00ss@gmail.com>
2022-03-22 01:05:32 +0300

    feat: removed default handlers

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -3,7 +3,6 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
 
-
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Привет, {message.from_user.full_name}!")
+    await message.answer(f"Привет, {message.from_user.full_name}! Я бот, который умеет скидывать домашнее задание. Чтобы узнать, что задали на сегодня, напиши /dz")
