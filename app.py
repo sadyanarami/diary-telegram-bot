f11931556e14f520afe603931c3b6456b1f2ddd1
wqross <wqr00ss@gmail.com>
2022-03-22 01:13:33 +0300

    feat: added default commands

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from aiogram import executor
 from loader import dp
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
+from utils.set_bot_commands import set_default_commands
+
 
 async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
+    await set_default_commands(dp)
 
 
 if __name__ == '__main__':
