dba34929b46c159b0086aaf657c74f641e76a4e3
sadyanarami <82324965+sadyanarami@users.noreply.github.com>
2022-03-22 18:43:04 +0300

    feat: load_dz.py, FSM

--- handlers/users/load_dz.py
+++ handlers/users/load_dz.py
@@ -1,10 +1,20 @@
+import pytesseract
+from PIL import Image
 from aiogram import types
-from aiogram.dispatcher import filters
-from loader import dp
+from aiogram.dispatcher import FSMContext
 
-@dp.message_handler(filters.Command('load_dz'), filters.AdminFilter())
-async def photo_handler(message: types.Message):
+from loader import dp
+from states.loadDZ import LoadDz
+
+
+@dp.message_handler(text='/load_dz')
+async def enter_photo_state(message: types.Message):
+    await message.answer('Загрузите фото')
+    await LoadDz.Q1.set()
+
+
+@dp.message_handler(content_types='photo', state=LoadDz.Q1)
+async def enter_photo(message: types.Message, state: FSMContext):
     await message.photo[-1].download('downloaded-photo/dz.jpg')
     await message.answer('Фото успешно получено!')
-
-    
+    await state.reset_state()
