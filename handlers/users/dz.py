0a8f8cb186531a941554cf474e455d25136ca432
wqross <wqr00ss@gmail.com>
2022-03-22 14:16:41 +0300

    feat: added /dz command

--- handlers/users/dz.py
+++ handlers/users/dz.py
@@ -2,6 +2,16 @@ from aiogram import types
 
 from loader import dp
 
+import pytesseract
+from PIL import Image
+
 @dp.message_handler(text="/dz")
 async def bot_start(message: types.Message):
-    await message.answer("Вот домашнее задание на сегодня:")
+    img = Image.open('downloaded-photo/dz.jpg')
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+    custom_config = r'--oem 3 --psm 6'
+
+    dz = pytesseract.image_to_string(img, lang='rus', config=custom_config)
+
+    await message.reply(dz)
